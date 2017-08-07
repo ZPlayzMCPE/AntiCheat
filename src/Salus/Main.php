5b2b71bc1db12906ff4399cd9d3cac3d55e25fcf
Driesboy <coenendries3@gmail.com>
2017-08-07 07:47:37 +0200

    Fixed some problems with fences

--- src/Salus/Main.php
+++ src/Salus/Main.php
@@ -232,18 +232,21 @@ class Main extends PluginBase implements Listener {
     $newPos = $event->getTo();
     if($this->getConfig()->get("detect-Fly") === true){
       if(!$player->isCreative() and !$player->isSpectator() and !$player->getAllowFlight()){
-        if ($oldPos->getY() <= $newPos->getY()){
-          if($player->GetInAirTicks() > 40){
-            $maxY = $player->getLevel()->getHighestBlockAt(floor($newPos->getX()), floor($newPos->getZ()));
-            if($newPos->getY() - 2 > $maxY){
-              $this->point[$player->getName()]["fly"] += (float) 1;
-              if((float) $this->point[$player->getName()]["fly"] > (float) 3){
-                $this->HackDetected($player, "Fly Hacks", "Salus", "1");
+        if( !in_array(Block::FENCE               , $this->surroundings )
+        and !in_array(Block::FENCE_GATE          , $this->surroundings )){
+          if ($oldPos->getY() <= $newPos->getY()){
+            if($player->GetInAirTicks() > 40){
+              $maxY = $player->getLevel()->getHighestBlockAt(floor($newPos->getX()), floor($newPos->getZ()));
+              if($newPos->getY() - 2 > $maxY){
+                $this->point[$player->getName()]["fly"] += (float) 1;
+                if((float) $this->point[$player->getName()]["fly"] > (float) 3){
+                  $this->HackDetected($player, "Fly Hacks", "Salus", "1");
+                }
               }
             }
+          }else{
+            $this->point[$player->getName()]["fly"] = (float) 0;
           }
-        }else{
-          $this->point[$player->getName()]["fly"] = (float) 0;
         }
       }
     }
